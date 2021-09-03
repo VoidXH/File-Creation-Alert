5b0ce504b5052128e586da2f1eb28fa13f2e063b
Bence Sgánetz <voidxb@gmail.com>
2021-09-04 00:35:41 +0200

    Version 1.1 with multiple directory handling

--- FileCreationAlert/Program.cs
+++ FileCreationAlert/Program.cs
@@ -7,6 +7,11 @@ using System.Threading;
 
 namespace FileCreationAlert {
     class Program {
+        static Dictionary<string, string> config;
+        static SmtpClient smtp;
+
+        static readonly object logLock = new object(), mailLock = new object();
+
         static Dictionary<string, string> GetConfig() {
             Dictionary<string, string> config = new Dictionary<string, string>();
             string[] source = File.ReadAllLines(configFileName);
@@ -20,29 +25,14 @@ namespace FileCreationAlert {
             return config;
         }
 
-        static void Main() {
-            if (!File.Exists(configFileName)) {
-                File.WriteAllText(configFileName, defaultConfig);
-                Console.WriteLine("Configuration file created. Please fill it with your data and relaunch this application!\n" +
-                    "Press any key (except power off/sleep/reset) to close this window!");
-                Console.ReadKey();
-                return;
-            }
+        static void Process(int thread, string path) {
+            lock (logLock)
+                Console.WriteLine($"[{thread}] Looking for file changes at {path}.");
 
-            Dictionary<string, string> config = GetConfig();
-            if (!Directory.Exists(config["path"])) {
-                Console.WriteLine("The path written in the configuration file is invalid. Please change that to a valid folder!\n" +
-                    "Press any key (except minimize or full scrren) to close this window!");
-                Console.ReadKey();
-                return;
-            }
-
-            Console.WriteLine("Configuration file loaded, looking for file changes at " + new DirectoryInfo(config["path"]).FullName);
-
-            string[] contents = Directory.GetFiles(config["path"], config["pattern"]);
+            string[] contents = Directory.GetFiles(path, config["pattern"]);
             while (true) {
                 Thread.Sleep(int.Parse(config["interval"]) * 1000);
-                string[] newContents = Directory.GetFiles(config["path"], config["pattern"]);
+                string[] newContents = Directory.GetFiles(path, config["pattern"]);
                 for (int n = 0; n < newContents.Length; ++n) {
                     bool exists = false;
                     for (int o = 0; o < contents.Length; ++o) {
@@ -53,30 +43,73 @@ namespace FileCreationAlert {
                     }
                     if (!exists) {
                         string fileName = new FileInfo(newContents[n]).Name;
-                        Console.WriteLine("New file: " + fileName);
+                        lock (logLock)
+                            Console.WriteLine($"[{thread}] New file: {fileName}.");
 
-                        SmtpClient smtp = new SmtpClient {
-                            Host = config["host"],
-                            Port = int.Parse(config["port"]),
-                            EnableSsl = true,
-                            Timeout = 10000,
-                            DeliveryMethod = SmtpDeliveryMethod.Network,
-                            UseDefaultCredentials = false,
-                            Credentials = new NetworkCredential(config["address"], config["password"])
-                        };
                         using (MailMessage message = new MailMessage(config["address"], config["address"], config["title"],
                             config["body"].Replace("%f", fileName))) {
                             try {
-                                smtp.Send(message);
-                                Console.WriteLine("E-mail sent.");
-                            } catch (Exception) {
-                                Console.WriteLine("Couldn't send e-mail, please check the configuration file!");
+                                lock (mailLock)
+                                    smtp.Send(message);
+                                lock (logLock)
+                                    Console.WriteLine($"[{thread}] E-mail sent.");
+                            } catch {
+                                lock (logLock)
+                                    Console.WriteLine($"[{thread}] Couldn't send e-mail, please check the configuration file!");
                             }
                         }
                     }
                 }
                 contents = newContents;
             }
+        }
+
+        static void Main() {
+            if (!File.Exists(configFileName)) {
+                File.WriteAllText(configFileName, defaultConfig);
+                Console.WriteLine("Configuration file created. Please fill it with your data and relaunch this application!\n" +
+                    "Press any key (except power off/sleep/reset) to close this window!");
+                Console.ReadKey();
+                return;
+            }
+
+            config = GetConfig();
+
+            smtp = new SmtpClient {
+                Host = config["host"],
+                Port = int.Parse(config["port"]),
+                EnableSsl = true,
+                Timeout = 10000,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(config["address"], config["password"])
+            };
+
+            int pathCount = 1;
+            List<Thread> threads = new List<Thread>();
+            while (config.ContainsKey("path" + pathCount)) {
+                if (Directory.Exists(config["path" + pathCount])) {
+                    int i = pathCount;
+                    threads.Add(new Thread(new ThreadStart(() => Process(i, new DirectoryInfo(config["path" + i]).FullName))));
+                    threads[threads.Count - 1].Start();
+                } else
+                    lock (logLock)
+                        Console.WriteLine($"The path \"{config["path" + pathCount]}\" is invalid.");
+                ++pathCount;
+            }
+
+            if (threads.Count == 0) {
+                Console.WriteLine("All paths were invalid, please fix them in the configuration file!\n" +
+                    "Press any key (except minimize or full screen) to close this window!");
+                Console.ReadKey();
+                return;
+            }
+
+            lock (logLock)
+                Console.WriteLine("Configuration file loaded.");
+
+            for (int i = 0; i < threads.Count; ++i)
+                threads[i].Join(); // Exit if all threads crash for some reason
         }
 
         const string configFileName = "config.ini";
@@ -92,7 +125,8 @@ body=File name: %f
 
 [Application]
 ; The folder to watch for changes, the default is the local folder
-path=.
+; Additional folders can be added like path2, path3, path4...
+path1=.
 ; Filter for single file type if needed
 pattern=*.*
 ; Folder checking interval in seconds
--- FileCreationAlert/Properties/AssemblyInfo.cs
+++ FileCreationAlert/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("FileCreationAlert")]
+[assembly: AssemblyTitle("File Creation Alert")]
 [assembly: AssemblyDescription("Checks for fresh created files and sends an e-mail when a new file is available.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("VoidX")]
-[assembly: AssemblyProduct("FileCreationAlert")]
+[assembly: AssemblyProduct("File Creation Alert")]
 [assembly: AssemblyCopyright("Copyright © VoidX 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("25b7936c-0f71-45a5-a227-eecdb06edc70")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
